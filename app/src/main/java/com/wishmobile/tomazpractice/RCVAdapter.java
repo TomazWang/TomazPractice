9b18bb8e6a9bfca0483da432b2b896fabc7aa8bc
Tomaz <tomaz@wishmobile.com>
2016-10-17 18:57:31 +0800

    - add item - remove item - 3 types

--- app/src/main/java/com/wishmobile/tomazpractice/RCVAdapter.java
+++ app/src/main/java/com/wishmobile/tomazpractice/RCVAdapter.java
@@ -1,32 +1,76 @@
 package com.wishmobile.tomazpractice;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.wishmobile.tomazpractice.data.DummyDatas;
 
 import java.util.ArrayList;
 
+import static android.content.ContentValues.TAG;
+
 /**
  * Created by TomazWang on 2016/10/17.
  */
 
-public class RCVAdapter extends RecyclerView.Adapter<RCVAdapter.ViewHolder>{
+public class RCVAdapter extends RecyclerView.Adapter<RCVAdapter.ViewHolder> {
+    private static final int TYPE_B = 0;
+    private static final int TYPE_A = 1;
     private final ArrayList<DummyDatas> data;
 
     public RCVAdapter(ArrayList<DummyDatas> dummyDates) {
         this.data = dummyDates;
+        Log.d(TAG, "RCVAdapter: data size = "+data.size());
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        if(position%2 == 0){
+            return TYPE_A;
+        }else{
+            return TYPE_B;
+        }
+
+
+
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+        Log.d(TAG, "onCreateViewHolder: creating view holder");
+        int layoutId = R.layout.item_dummydata_for_listview;
+
+        if(viewType == TYPE_B){
+            layoutId = R.layout.item_dummydata_for_listview_reverse;
+        }
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(layoutId, parent, false);
+
+        if(viewType == TYPE_A){
+            view.setBackgroundColor(Color.CYAN);
+        }
+
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+
+
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+
+        DummyDatas dummyData = data.get(position);
+
+        holder.txtTitle.setText(dummyData.getTitle());
+        holder.txtName.setText(dummyData.getName());
 
     }
 
@@ -35,16 +79,39 @@ public class RCVAdapter extends RecyclerView.Adapter<RCVAdapter.ViewHolder>{
         return data.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public void addItem(DummyDatas dummyDatas) {
+        int position = data.size();
+        data.add(dummyDatas);
+        this.notifyItemChanged(position);
+    }
+
+    public void removeLastItem() {
+        int position = data.size()-1;
+        data.remove(position);
+        this.notifyItemRemoved(position);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView txtTitle;
         TextView txtName;
-
-
+        ImageView img;
 
 
         public ViewHolder(View itemView) {
             super(itemView);
+
+            this.txtTitle = (TextView) itemView.findViewById(R.id.txt_title);
+            this.txtName = (TextView) itemView.findViewById(R.id.txt_name);
+            this.img = (ImageView)itemView.findViewById(R.id.iv_img);
+
+            img.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // TODO: click to change content;
+                }
+            });
+
         }
     }
 }
--- app/src/main/java/com/wishmobile/tomazpractice/fragment/RecyclerViewFragment.java
+++ app/src/main/java/com/wishmobile/tomazpractice/fragment/RecyclerViewFragment.java
@@ -4,11 +4,13 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.wishmobile.tomazpractice.R;
 import com.wishmobile.tomazpractice.RCVAdapter;
@@ -30,14 +32,6 @@ public class RecyclerViewFragment extends Fragment {
     @BindView(R.id.list_recycler)
     RecyclerView mRecyclerView;
 
-    @BindView(R.id.btn_linear)
-    Button linearBtn;
-
-    @BindView(R.id.btn_grid)
-    Button gridBtn;
-
-    @BindView(R.id.btn_flow)
-    Button flowBtn;
 
 
     private ArrayList<DummyDatas> mDummyDates = new ArrayList<>();
@@ -50,9 +44,13 @@ public class RecyclerViewFragment extends Fragment {
     // layout type
     public static final int LINER_LAYOUT = 0;
     public static final int GRID_LAYOUT = 1;
-    private static final String KEY_LAYOOUT_MABAGER_TYPE = "layoutManagerType";
+    public static final int FLOW_LAYOUT = 2;
 
-    @IntDef({LINER_LAYOUT, GRID_LAYOUT})
+    private static final String KEY_LAYOOUT_MABAGER_TYPE = "layoutManagerType";
+    private int mSpanCount = 4;
+
+
+    @IntDef({LINER_LAYOUT, GRID_LAYOUT, FLOW_LAYOUT})
     public @interface LayoutManagerType {}
 
     @LayoutManagerType
@@ -87,38 +85,89 @@ public class RecyclerViewFragment extends Fragment {
 
 
 
+
         return view;
     }
 
     private void initData() {
 
         for(int i=0; i<8; i++){
-            String title = "Title "+i;
-            String name = "Name "+i;
+            String title = "=Title "+i;
+            String name = "-Name "+i;
             mDummyDates.add(new DummyDatas(title, name, R.drawable.analytics));
         }
+
+        mAdapter = new RCVAdapter(mDummyDates);
     }
 
 
     private void createRecyclerView() {
+        mRecyclerView.setAdapter(mAdapter);
+        setRecyclerViewLayoutManager(mCurrentLayoutManagerType);
+    }
 
-        mAdapter = new RCVAdapter(mDummyDates);
+
+    private void setRecyclerViewLayoutManager(@LayoutManagerType int type) {
 
 
+        switch (type){
+            case GRID_LAYOUT:
+                mLayoutManager = new GridLayoutManager(getActivity(),mSpanCount);
+                mCurrentLayoutManagerType = GRID_LAYOUT;
+
+                break;
+
+            case FLOW_LAYOUT:
+                mLayoutManager = new StaggeredGridLayoutManager(mSpanCount, StaggeredGridLayoutManager.HORIZONTAL);
+                mCurrentLayoutManagerType = FLOW_LAYOUT;
+                break;
+
+            case LINER_LAYOUT:
+            default:
+                mLayoutManager = new LinearLayoutManager(getActivity());
+                mCurrentLayoutManagerType = LINER_LAYOUT;
+
+                break;
+
+
+        }
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
     }
 
 
     @OnClick(R.id.btn_add)
     public void addItem(){
+        // TODO: add item
+        int id = mDummyDates.size();
+        mAdapter.addItem(new DummyDatas("=Title "+id, "-Name "+id, R.drawable.analytics));
 
     }
 
 
     @OnClick(R.id.btn_remove)
     public void removeItem(){
-
+        // TODO: remove item
+        mAdapter.removeLastItem();
     }
+
+    @OnClick(R.id.btn_linear)
+    public void changeToLinear(){
+        setRecyclerViewLayoutManager(LINER_LAYOUT);
+    }
+
+    @OnClick(R.id.btn_grid)
+    public void changeToGrid(){
+        setRecyclerViewLayoutManager(GRID_LAYOUT);
+    }
+
+    @OnClick(R.id.btn_flow)
+    public void changeToFlow(){
+        setRecyclerViewLayoutManager(FLOW_LAYOUT);
+    }
+
+
 
 
 
